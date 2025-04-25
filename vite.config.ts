11b29fd813d0fa879994d02c861ef28ed03920c5
Arthur Grossi <arthurgrossi1990@gmail.com>
2025-04-25 20:58:11 -0300

    hosp-vercel-3

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve( __dirname, 'src/index.html'),  // <-- aponto pro novo index
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  build: {
-    outDir: 'dist', // <- Adiciona essa linha!
-  },
-})
+});
